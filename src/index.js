73d9a0f42b924b610fdca134f39bd6ca8a5eaecb
yeojongki <kk597@sina.com>
2018-08-20 22:00:58 +0800

    del: some useless file; add: some file

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+// import registerServiceWorker from './registerServiceWorker';
+
+class App extends Component {
+  render() {
+    return <div className="App">App</div>;
+  }
+}
 
 ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+// registerServiceWorker();
